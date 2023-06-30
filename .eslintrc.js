f7c3689a2a01c2fefb2f73a68c99af9f8d23b90e
Peter Park <adsl1664@gmail.com>
2023-06-30 17:16:45 +0900

    change: config 정보 프로젝트 내부 이동

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,68 @@
 module.exports = {
-  ...require('@cherr0/fe-config-package/eslint/next'),
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: [
+    'next',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier'
+  ],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: './tsconfig.json',
     sourceType: 'module'
-  }
+  },
+  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  },
+  rules: {
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function'
+      }
+    ],
+    'react-hooks/exhaustive-deps': 'off',
+    '@next/next/no-html-link-for-pages': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@next/next/no-img-element': 'off',
+    'simple-import-sort/exports': 'warn',
+    'import/order': [
+      'warn',
+      {
+        groups: ['builtin', 'external', ['parent', 'sibling'], 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before'
+          }
+        ],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true
+        },
+        'newlines-between': 'always'
+      }
+    ]
+  },
+  ignorePatterns: [
+    '**/*.js',
+    '**/*.json',
+    'node_modules',
+    'public',
+    'styles',
+    '.next',
+    'dist'
+  ]
 }

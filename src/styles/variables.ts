4ace36c87ada7f923c74a0acd229f9b6c6632089
Peter Park <adsl1664@gmail.com>
2023-07-02 16:34:19 +0900

    add: color token 정보 추가

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -2,7 +2,9 @@ import { css } from 'styled-components'
 
 export const commonVariables = css`
   --text-default: #000000;
+  --text-weak: #666d77;
   --surface-default: #f7f7f5;
+  --border-default: #e2e8f0;
 `
 
 export const lightVariables = css`

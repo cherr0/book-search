ae3ac90c35bd72f6838577795df402e263580099
Peter Park <adsl1664@gmail.com>
2023-07-02 18:42:45 +0900

    add: Card 클릭 시 Detail 페이지로 넘어가도록 설정

--- pages/src/domain/features/book-search-list/index.tsx
+++ pages/src/domain/features/book-search-list/index.tsx
@@ -1,10 +1,18 @@
+import { useRouter } from 'next/router'
 import { InView } from 'react-intersection-observer'
 
 import useSearchQuery from '../../hooks/useSearchQuery'
 import PreviewList from '../../ui/preview-list'
 
+import { InternalPath } from '~/constants/route'
+
 const BookSearchList = () => {
   const { searchBookList, fetchNextPage, hasNextPage } = useSearchQuery()
+  const { push } = useRouter()
+
+  const handleCardClick = (bookId: string) => {
+    push(InternalPath.DETAIL + bookId)
+  }
 
   return (
     <PreviewList>
@@ -14,6 +22,7 @@ const BookSearchList = () => {
           title={title}
           subtitle={subtitle}
           thumbnailUrl={image}
+          onClick={() => handleCardClick(isbn13)}
         />
       ))}
       {hasNextPage && (
